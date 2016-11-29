f8bd61db04b4d965d2fac6c290de624c90e5c718
Lui <arai_lui@lob-inc.com>
2016-11-29 06:56:27 +0000

    Message-board

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -20,6 +20,6 @@ class MessagesController < ApplicationController
   private
   
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :body, :age)
   end
 end
