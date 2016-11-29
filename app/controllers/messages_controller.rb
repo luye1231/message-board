9587c9e985b05791e0a4cf79416988fb10db4b0f
Lui <arai_lui@lob-inc.com>
2016-11-29 08:09:46 +0000

    message-baord

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: [:edit, :update, :destroy]
-
+  
   def index
     @message = Message.new
     @messages = Message.all
@@ -18,6 +18,16 @@ class MessagesController < ApplicationController
     end
   end
   
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
   def destroy
     @message.destroy
     redirect_to root_path, notice: 'メッセージを削除しました'
@@ -25,8 +35,13 @@ class MessagesController < ApplicationController
   ## ここまで
   
   private
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
   
   def message_params
     params.require(:message).permit(:name, :body, :age)
   end
+  
 end
